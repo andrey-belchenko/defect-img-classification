866876698d8e92666bcafcf7bf473b442d02ac73
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2025-11-04 12:18:06 +0300

    vue hello world

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// App component with script setup
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
+
